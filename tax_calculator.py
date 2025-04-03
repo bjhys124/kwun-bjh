1491d8f91612d2b25515132584bea6ccaedf5a59
bjhys124 <fhzk1022@naver.com>
2025-04-03 21:59:24 +0900

    Delete tax_calculator.py

--- tax_calculator.py
+++
@@ -1,7 +0,0 @@
-def calculate_tax(df):
-    total_income = df[df['분류'] == '매출']['금액'].sum()
-    total_expense = df[df['분류'] != '매출']['금액'].sum()
-    vat_estimate = max((total_income - total_expense) * 0.1, 0)
-    income_tax_base = max((total_income - total_expense - 1500000), 0)
-    income_tax_estimate = income_tax_base * 0.06
-    return int(vat_estimate), int(income_tax_estimate)
